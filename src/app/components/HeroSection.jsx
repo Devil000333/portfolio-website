2626d7b06b8bf34ea85cf1346ab2ddc503469f3c
Devil000333 <97149818+Devil000333@users.noreply.github.com>
2024-11-27 23:03:25 +0530

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -35,8 +35,8 @@ const HeroSection = () => {
                         />
                     </h1>
                     <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lf:text-xl">
-                        Hey this is dhanraj and I'm improvising this page. 
-                       Looking forward for your review.
+                        {"Hey this is dhanraj and I'm improvising this page."}
+                        {"Looking forward for your review."}
                     </p> 
                     <div>
                         <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
