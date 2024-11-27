9782cd3d59dd8b9740cdc52474ddcd3dc88c5975
Devil000333 <97149818+Devil000333@users.noreply.github.com>
2024-11-27 23:09:06 +0530

    Update Footer.jsx

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
         <div className="container p-12 flex justify-between">
-            <Image src="/images/dan-icon.png" alt="dan icon" width={100} height={100}/>
+            <Image src="/images/dan-icon.png" alt="" width={100} height={100}/>
             <p className="text-slate-600">{"All rights reserved."}</p>
         </div>
     </footer>
