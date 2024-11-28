1c3a0cf4ebd2e43a5dc6d4442bc153e2cf345a60
Devil000333 <97149818+Devil000333@users.noreply.github.com>
2024-11-28 11:44:20 +0530

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -40,14 +40,18 @@ const HeroSection = () => {
                         {"If you got any suggestion, I'm looking forward for it. 😄"}
                     </p> 
                     <div>
-                        <button className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+                        <Link 
+                            href="/#contact"
+                            className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
                             Hire Me
-                        </button>
-                        <button className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white border border-white mt-3">
+                        </Link>
+                        <Link
+                            href="https://drive.google.com/drive/folders/1L0rdd51rF3ThrEhmU6CdAghQYVHHV_tZ?usp=sharing"
+                            className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white border border-white mt-3">
                             <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                                 Download CV
                             </span>
-                        </button>
+                        </Link>
                     </div>
                 </motion.div>
                 <motion.div
