5847354b8c344e4c4361c4cc873cc271cf66bf78
Devil000333 <97149818+Devil000333@users.noreply.github.com>
2024-11-28 11:09:53 +0530

    Update TabButton.jsx

--- src/app/components/TabButton.jsx
+++ src/app/components/TabButton.jsx
@@ -4,14 +4,14 @@ import { motion } from "framer-motion";
 const variants = {
     default: { width: 0},
     active: { width: "calc(100% - 0.75rem)" },
-}
+};
 const TabButton = ({active, selectTab, children}) => { 
     const buttonClasses = active 
-        ? 'text-white border-b border-purple-500' 
-        : 'text-[#ADB7BE]'
+        ? "text-white border-b border-purple-500" 
+        : "text-[#ADB7BE]";
     return (
         <button onClick={selectTab}> 
-            <p className={"mr-3 font-semibold hover:text-white ${buttonClaases}"}
+            <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}
             >
                 {children}
             </p>
