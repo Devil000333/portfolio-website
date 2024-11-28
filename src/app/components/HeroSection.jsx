97fb42e0971173557304e13da4036c0d0d17301f
Devil000333 <97149818+Devil000333@users.noreply.github.com>
2024-11-28 11:53:26 +0530

    Update HeroSection.jsx

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -44,12 +44,14 @@ const HeroSection = () => {
                     <div>
                         <Link 
                             href="/#contact"
-                            className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white">
+                            className="px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-200 text-white"
+                            >
                             Hire Me
                         </Link>
                         <Link
                             href="https://drive.google.com/drive/folders/1L0rdd51rF3ThrEhmU6CdAghQYVHHV_tZ?usp=sharing"
-                            className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white border border-white mt-3">
+                            className="px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white border border-white mt-3"
+                            >
                             <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                                 Download CV
                             </span>
